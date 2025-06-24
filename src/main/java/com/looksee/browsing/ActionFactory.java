6ed297146cdd3cd584d61e6064e20159fe746e40
Brandon Kindred <brandon.kindred@gmail.com>
2025-06-24 00:26:34 -0400

    fix: merged some core code from journey executor

---
+++ src/main/java/com/looksee/browsing/ActionFactory.java
@@ -0,0 +1,70 @@
+package com.looksee.browsing;
+
+import com.looksee.models.enums.Action;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Constructs {@linkplain Actions} provided by Selenium
+ *
+ */
+public class ActionFactory {
+	@SuppressWarnings("unused")
+	private static Logger log = LoggerFactory.getLogger(ActionFactory.class);
+
+	private Actions builder;
+	
+	public ActionFactory(WebDriver driver){
+		builder = new Actions(driver);
+	}
+
+	/**
+	 * Executes an action on a web element
+	 * @param elem the web element to perform the action on
+	 * @param input the input to send to the web element
+	 * @param action the action to perform
+	 * @throws WebDriverException if the action cannot be performed
+	 */
+	public void execAction(WebElement elem, String input, Action action) throws WebDriverException{
+		if(Action.CLICK.equals(action)){
+			builder.click(elem);
+		}
+		else if(Action.CLICK_AND_HOLD.equals(action)){
+			builder.clickAndHold(elem);
+		}
+		//Context click clicks select/options box
+		else if(Action.CONTEXT_CLICK.equals(action)){
+			builder.contextClick(elem);
+		}
+		else if(Action.DOUBLE_CLICK.equals(action)){
+			builder.doubleClick(elem);
+		}
+		/*else if("dragAndDrop".equals(action)){
+			//builder.dragAndDrop(source, target);
+		}
+		else if("keyDown".equals(action)){
+			//builder.keyDown();
+		}
+		else if("keyUp".equals(action)){
+			//builder.keyUp(theKey);
+		}
+		*/
+		else if(Action.RELEASE.equals(action)){
+			builder.release(elem);
+		}
+		else if(Action.SEND_KEYS.equals(action)){
+			//builder.sendKeys(elem, Keys.chord(Keys.CONTROL, Keys.ALT, Keys.DELETE));
+			builder.sendKeys(elem, input);
+		}
+		else if(Action.MOUSE_OVER.equals(action)){
+			builder.scrollToElement(elem);
+			builder.moveToElement(elem);
+		}
+
+		builder.build().perform();
+	}
+}
---
+++ src/main/java/com/looksee/browsing/RateLimitExecutor.java
@@ -0,0 +1,51 @@
+package com.looksee.browsing;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.Command;
+import org.openqa.selenium.remote.HttpCommandExecutor;
+import org.openqa.selenium.remote.Response;
+
+/**
+ * A custom command executor that limits the number of actions per second
+ */
+public class RateLimitExecutor extends HttpCommandExecutor {
+
+    public static final int CONCURRENT_SESSIONS = 1;
+    public static final int ACTIONS_RATE_LIMIT_PER_SECOND = 50;
+
+    public static final double SECONDS_PER_ACTION = ((double) CONCURRENT_SESSIONS)
+            / ((double) ACTIONS_RATE_LIMIT_PER_SECOND);
+    private long lastExecutionTime;
+
+    /**
+     * Creates a new RateLimitExecutor
+     * @param addressOfRemoteServer the address of the remote server
+     */
+    public RateLimitExecutor(URL addressOfRemoteServer) {
+        super(addressOfRemoteServer);
+        lastExecutionTime = 0;
+    }
+
+    /**
+     * Executes a command
+     * @param command the command to execute
+     * @return the response
+     * @throws IOException if an I/O error occurs
+     */
+    public Response execute(Command command) throws IOException {
+        long currentTime = Instant.now().toEpochMilli();
+        double elapsedTime = TimeUnit.MILLISECONDS.toSeconds(currentTime - lastExecutionTime);
+        if (elapsedTime < SECONDS_PER_ACTION) {
+            try {
+                Thread.sleep(TimeUnit.SECONDS.toMillis((long)(SECONDS_PER_ACTION - elapsedTime)));
+            } catch (InterruptedException e) {
+                //e.printStackTrace();
+            }
+        }
+        lastExecutionTime = Instant.now().toEpochMilli();
+        return super.execute(command);
+    }
+}
