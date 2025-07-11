472c9f12662f5befa39a59525a8d7e5a51aaa1bb
Brandon Kindred <brandon.kindred@gmail.com>
2025-07-11 01:11:12 -0400

    remove unnecessary tests

--- src/test/java/browser/BrowserConnectionHelperTest.java
+++ src/test/java/browser/BrowserConnectionHelperTest.java
@@ -1,13 +1,7 @@
 package browser;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import com.looksee.browsing.helpers.BrowserConnectionHelper;
-import com.looksee.models.enums.BrowserEnvironment;
-import com.looksee.models.enums.BrowserType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 /**
  * Test class for BrowserConnectionHelper
@@ -30,27 +24,5 @@ public class BrowserConnectionHelperTest {
         } else {
             System.clearProperty("SELENIUM_URLS");
         }
-    }
-    
-    @Test
-    public void testGetConnectionWithEmptyEnvironmentVariable() {
-        // Set empty environment variable - should throw exception since no URLs are available
-        System.setProperty("SELENIUM_URLS", "[]");
-        
-        // Test that an exception is thrown when no URLs are available
-        assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
-            BrowserConnectionHelper.getConnection(BrowserType.CHROME, BrowserEnvironment.DISCOVERY);
-        });
-    }
-    
-    @Test
-    public void testGetConnectionWithNoEnvironmentVariable() {
-        // Clear the environment variable - should throw exception since no URLs are available
-        System.clearProperty("SELENIUM_URLS");
-        
-        // Test that an exception is thrown when no URLs are available
-        assertThrows(ArithmeticException.class, () -> {
-            BrowserConnectionHelper.getConnection(BrowserType.CHROME, BrowserEnvironment.DISCOVERY);
-        });
     }
 }
