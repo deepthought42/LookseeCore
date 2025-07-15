d4e61984414b7a313312dbad87c600786af400c9
Brandon Kindred <brandon.kindred@gmail.com>
2025-07-15 12:06:28 -0400

    create test profile

--- src/test/java/com/looksee/LookseeCoreAutoConfigurationTest.java
+++ src/test/java/com/looksee/LookseeCoreAutoConfigurationTest.java
@@ -7,15 +7,23 @@ import com.looksee.services.DomainService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.test.context.ActiveProfiles;
 
 /**
  * Test class to verify that LookseeCoreAutoConfiguration properly registers
  * core services as Spring beans (excluding repository beans).
  */
+@ActiveProfiles("test")
 class LookseeCoreAutoConfigurationTest {
 
     private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
-            .withConfiguration(AutoConfigurations.of(LookseeCoreAutoConfiguration.class));
+            .withConfiguration(AutoConfigurations.of(LookseeCoreAutoConfiguration.class))
+            .withPropertyValues(
+                "spring.cloud.gcp.pubsub.enabled=false",
+                "spring.cloud.gcp.project-id=test-project",
+                "management.health.pubsub.enabled=false",
+                "logging.level.org.springframework.cloud.gcp=OFF"
+            );
 
     @Test
     void shouldRegisterServices() {
