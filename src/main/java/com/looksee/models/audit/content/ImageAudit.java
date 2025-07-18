83a5b4e77ee5e41465580de6484159ff6abe4e8b
Brandon Kindred <brandon.kindred@gmail.com>
2025-07-18 13:08:03 -0400

    fix: removed visual audit classes

--- src/main/java/com/looksee/models/audit/content/ImageAudit.java
+++ src/main/java/com/looksee/models/audit/content/ImageAudit.java
@@ -1,8 +1,23 @@
-package com.looksee.models.audit;
+package com.looksee.models.audit.content;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.looksee.models.ElementState;
 import com.looksee.models.ImageElementState;
 import com.looksee.models.PageState;
+import com.looksee.models.audit.Audit;
+import com.looksee.models.audit.AuditRecord;
+import com.looksee.models.audit.IExecutablePageStateAudit;
+import com.looksee.models.audit.Score;
+import com.looksee.models.audit.StockImageIssueMessage;
+import com.looksee.models.audit.UXIssueMessage;
 import com.looksee.models.designsystem.DesignSystem;
 import com.looksee.models.enums.AuditCategory;
 import com.looksee.models.enums.AuditLevel;
@@ -12,17 +27,11 @@ import com.looksee.models.enums.Priority;
 import com.looksee.services.AuditService;
 import com.looksee.services.UXIssueMessageService;
 import com.looksee.utils.BrowserUtils;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+
 import lombok.NoArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 /**
- * Responsible for executing an audit on the hyperlinks on a page for the information architecture audit category
+ * Responsible for executing an audit on the images on a page for the imagery audit category
  */
 @Component
 @NoArgsConstructor
